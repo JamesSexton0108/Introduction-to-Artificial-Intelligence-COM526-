59b0008508ba1d62d253da9176fb847055ae4862
James Sexton <0sextj12@solent.ac.uk>
2025-09-24 01:48:17 +0100

    Completed A* algorithm

--- pathfinding.py
+++ pathfinding.py
@@ -9,24 +9,50 @@ def a_star(maze, start, goal):
     heapq.heappush(queue, (0, start))
     # Providing some code for you as they will be helpful shortly
     directions = {
-        "right": (0, 1),
-        "left": (0, -1),
-        "up": (-1, 0),
-        "down": (1, 0)
+        "right": (1, 0),
+        "left": (-1, 0),
+        "up": (0, 1),
+        "down": (0, -1)
     }
     predecessors = {start: None}    # Enables the get_path function to backtrack
     g_values = {start: 0}   # The g score for each cell
 
     # loop through the queue, this is an infinite loop that will only stop if the queue is empty
-        ##current_cell = # get this from the priority queue
+    while queue:
+        current_f, current_cell = heapq.heappop(queue)   #get this from the priority queue
 
-        # Check if the current cell is the goal:
-            # if it is, run this command:
-            # return get_path(predecessors, start, goal)
+        # Check if the current cell is the goal
+        if current_cell == goal:
+                # if it is, run this command:
+            return get_path(predecessors, start, goal)
 
-        # Now lets look at where we can move to from the current cell.
+    # Now lets look at where we can move to from the current cell.
+
+
         # For each direction do the following:
-            # Figure out the coordinates of the neighbouring cell - the offsets are provided above.
+        cx, cy = current_cell
+        for name, (dx, dy) in directions.items():
+            nx, ny = cx + dx, cy + dy
+            neighbour = (nx, ny)
+
+            height = len(maze)
+            width = len(maze[0]) if height > 0 else 0
+            in_bounds = 0 <= nx < width and 0 <= ny < height
+            if not in_bounds:
+                continue
+            if maze[ny][nx] == 'x':
+                continue
+            if neighbour in g_values:
+                continue
+
+
+
+
+
+
+
+
+# Figure out the coordinates of the neighbouring cell - the offsets are provided above.
             # For example, if the direction is 'up' then you would deduct 1 from the y coordinate
 
             # Check that this neighbouring cell is actually a valid move.
@@ -38,22 +64,28 @@ def a_star(maze, start, goal):
                 # Our current cell and its cost should be stored in the dictionary g_values
                 # Retrieve that value - add 1 to it and we have out cost for the neighbouring cell
 
-                # Add the neighbouring cell and its cost to the g_values dictionary,
-                # Where the (x, y) coordinates are the key and the cost is the value
+            # Add the neighbouring cell and its cost to the g_values dictionary,
+            # Where the (x, y) coordinates are the key and the cost is the value
+            parent_g = g_values[current_cell]
+            cost = parent_g + 1
+            g_values[neighbour] = cost
 
-                # Now calculate the H score.
-                # In the utils.py file there is a manhatten_distance function.
-                # Use this to calculate the distance between the neighbouring cell and the goal.
-                # The value it returns will be the H score
+            # Now calculate the H score.
+            # In the utils.py file there is a manhatten_distance function.
+            # Use this to calculate the distance between the neighbouring cell and the goal.
+            # The value it returns will be the H score
+            h = utils.manhattan_distance(neighbour, goal)
 
-                # Using that we can calculate the overall F score by adding the cost and the H score
+            # Using that we can calculate the overall F score by adding the cost and the H score
+            f = cost + h
 
-                # Now we have its F-Score we can add this neighbouring cell (a viable move) to our priority queue
-                # You should add the cell coords (x, y) to the queue and the prioirty value should be the f-score
+            # Now we have its F-Score we can add this neighbouring cell (a viable move) to our priority queue
+            # You should add the cell coords (x, y) to the queue and the prioirty value should be the f-score
+            heapq.heappush(queue, (f, neighbour))
 
-                # Allows the get_path function to backtrack later, do not change
-                ##predecessors[neighbour] = current_cell
-    ##return None
+            # Allows the get_path function to backtrack later, do not change
+            predecessors[neighbour] = current_cell
+    return None
 
 
 def get_path(predecessors, start, goal):
--- tui.py
+++ tui.py
@@ -1,5 +1,6 @@
 import pathfinding
 import utils
+import time
 
 
 def display_map(maze):
@@ -9,19 +10,32 @@ def display_map(maze):
 
 
 def show_path(maze, path):
+    grid = [row[:] for row in maze]
+    if not path:
+        print("No path found.")
+        return
+
+    for step_idx, (x, y) in enumerate(path):
+        if grid[y][x] not in ("s","g"):
+            grid[y][x] = '-'
+
+        display_map(grid)
+        print(f"\nStep {step_idx + 1}/{len(path)} at {(x, y)}\n")
+        time.sleep(0.1)
     # We are going to show the path the A* took one step at a time
     # for each coordinate provided in path:
         # print the map, showing a dash at that coordinate.
-        # However, do not overwrite the start and goal, these should continue to be displayed as 's' and 'g'
-    pass # DELETE THIS LINE!
+        # However, do not overwrite the start and goal, these should continue to be displayed as 's' and 'g' # DELETE THIS LINE!
 
 
 if __name__ == "__main__":
     maze_map = utils.import_maze("mazes/maze1.txt")
     start = utils.locate(maze_map, 's')
     goal = utils.locate(maze_map, 'g')
-    display_map(maze_map)
-    print(utils.locate(maze_map, 's'))
-    print(utils.locate(maze_map, 'g'))
+    #display_map(maze_map)
+    #print(utils.locate(maze_map, 's'))
+    #print(utils.locate(maze_map, 'g'))
+    path = pathfinding.a_star(maze_map, start, goal)
+    show_path(maze_map, path)
 
     show_path(maze_map, pathfinding.a_star(maze_map, start, goal))
